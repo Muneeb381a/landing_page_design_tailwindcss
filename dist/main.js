42b206306ba64b527e01e308694365ece349a1ca
Muneeb381a <muneebafzal381a@gmail.com>
2024-09-18 16:44:04 +0500

    creating services page review page news letter and footer

--- dist/main.js
+++ dist/main.js
@@ -13,3 +13,29 @@ navLink.forEach((link) => {
     hamburger.classList.toggle("ri-close-large-line");
   });
 });
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
